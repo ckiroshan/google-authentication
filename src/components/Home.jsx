bc62121d34474170fcee0e8f4d0ed52f1fdf024e
ckiroshan <ckiroshan@gmail.com>
2025-04-19 07:18:15 +0530

    feat: update App to integrate 2 components routing

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Home Page</h1>
+        <p>This is the Home redirected from Login Page.</p>
+        <button>Log Out</button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <h1>Login Page</h1>
+      <p>This is the Login page.</p>
+      <button>Log In</button>
+    </div>
+  );
+};
+
+export default Login;
