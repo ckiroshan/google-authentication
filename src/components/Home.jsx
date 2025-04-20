83fe5d0fc92447b0b7bab70066060dabf6792138
ckiroshan <ckiroshan@gmail.com>
2025-04-20 08:44:53 +0530

    feat: add navigation to redirect to dashboard after successful sign-in

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="container">
         <h1>Home Page</h1>
-        <p>This is the Home redirected from Login Page.</p>
+        <p>This is the Homepage. Click below to login.</p>
         <GoogleLogin
           onSuccess={(credentialResponse) => {
             console.log(credentialResponse);
             console.log(jwtDecode(credentialResponse.credential));
+            navigate("/dashboard");
           }}
           onError={() => alert("Login failed, Try again.")}
         />
