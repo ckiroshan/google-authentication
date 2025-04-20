650ead7a13bcf4ab9e3337d47dc747468770fc6f
ckiroshan <ckiroshan@gmail.com>
2025-04-20 08:06:30 +0530

    feat: add Google OAuth provider to app root

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const CLIENT_ID = "838408993225-8vo2td6lchuksufn6jfe9l3gmed25ui3.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
