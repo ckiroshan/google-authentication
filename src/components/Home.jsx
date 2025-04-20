86225627f6bb16572e493d29e84aab1dddcfec66
ckiroshan <ckiroshan@gmail.com>
2025-04-20 08:11:29 +0530

    feat: add Google Login button to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 const Home = () => {
   return (
@@ -6,7 +8,13 @@ const Home = () => {
       <div className="container">
         <h1>Home Page</h1>
         <p>This is the Home redirected from Login Page.</p>
-        <button>Log Out</button>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+            console.log(jwtDecode(credentialResponse.credential));
+          }}
+          onError={() => alert("Login failed, Try again.")}
+        />
       </div>
     </>
   );
