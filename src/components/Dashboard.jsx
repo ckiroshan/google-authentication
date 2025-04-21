e0b27f4a79749c37dce726b1523516f9ee20a2de
ckiroshan <ckiroshan@gmail.com>
2025-04-21 07:18:54 +0530

    feat: add logout function & redirection to Home component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { googleLogout } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    googleLogout();
+    navigate("/"); // Redirect to home after logout
+    console.log("Logged out successfully");
+  };
+
   return (
     <div className="container">
       <h1>Dashboard Page</h1>
       <p>This is the Dashboard page.</p>
-      <button>Log out</button>
+      <button className="btn" onClick={handleLogout}>Log out</button>
     </div>
   );
 };
